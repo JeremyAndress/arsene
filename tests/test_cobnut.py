403ac5b7e4daf1a388aaf7f2751fb092b5327b8d
JeremyAndress <jeremysilvasilva@gmail.com>
2021-01-21 12:58:58 -0300

    :white_check_mark: Add delete test

--- tests/test_cobnut.py
+++ tests/test_cobnut.py
@@ -2,9 +2,11 @@ from cobnut import __version__, Cobnut, RedisModel
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.1'
 
 def test_client():
     cobnut = Cobnut(redis_connection=RedisModel(host="localhost"))
     cobnut.set(key='test', data='test')
+    cobnut.get(key='test')
+    cobnut.delete(key='test')
     cobnut.get(key='test')
