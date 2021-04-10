c0c429a3eda1fe223b6c43ca6fc85438d70ae021
JeremySilvaSilva <jeremysilvasilva@gmail.com>
2021-04-10 11:19:10 -0400

    :sparkles: Add ssl attributes to RedisModel

--- arsene/schemas/redis.py
+++ arsene/schemas/redis.py
@@ -4,10 +4,17 @@ from pydantic import BaseModel
 
 class RedisModel(BaseModel):
     host: str
-    port: int = 6379
     db: int = 0
+    port: int = 6379
+    ssl: bool = False
+    encoding: str = 'utf-8'
+    health_check_interval: int = 0
+    username: Optional[str] = None
     password: Optional[str] = None
+    ssl_check_hostname: bool = False
+    ssl_ca_certs: Optional[str] = None
     max_connections: Optional[int] = None
+    ssl_cert_reqs: Optional[str] = 'required'
     socket_connect_timeout: Optional[int] = None
 
     class Config:
